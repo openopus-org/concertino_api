332d17db692d4697408fae7d3d1d94c8266123c6
Adriano Brandao <adrianobrandao@Adrianos-MacBook-Pro.local>
2019-09-01 19:05:40 -0300

    Versioning

--- lib/ini.php
+++ lib/ini.php
@@ -13,7 +13,7 @@
   // global constants
 
   define ("SOFTWARENAME", "Concertino");
-  define ("SOFTWAREVERSION", "1.19.08");
+  define ("SOFTWAREVERSION", "1.19.09");
   define ("USERAGENT", SOFTWARENAME. "/" . SOFTWAREVERSION. " ( ". SOFTWAREMAIL. " )");
   define ("RECRETS", 60);
   define ("MIN_SIMILAR", 40);
