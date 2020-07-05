bbff84ba8655ef87f016d170835d8fce9473e062
Open Opus <concertino@openopus.org>
2020-07-04 22:09:13 -0300

    Free search doesn't work with null composers

--- lib/lib.php
+++ lib/lib.php
@@ -37,7 +37,7 @@
     {
       unset ($performers);
 
-      if (in_array ("Classical", $alb["attributes"]["genreNames"]) || in_array ("Opera", $alb["attributes"]["genreNames"]))
+      if ($alb["attributes"]["composerName"] && in_array ("Classical", $alb["attributes"]["genreNames"]) || in_array ("Opera", $alb["attributes"]["genreNames"]))
       {
         $alb["artists"] = preg_split("/(\,|\&)/", $alb["attributes"]["artistName"]);
 
